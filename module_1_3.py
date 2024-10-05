9fe8bd9b792013085e24d98ef912c7997bff5e79
Fedor-snow <117363957+Fedor-snow@users.noreply.github.com>
2024-10-05 13:01:42 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,8 @@
 name = "Fedor"
 print(name, type(name))
-age = 18
-print(age, type(age))
 age = 17
+print(age, type(age))
+age = age + 1
 print(age, type(age))
 is_student = True
 print(is_student, type(is_student))
